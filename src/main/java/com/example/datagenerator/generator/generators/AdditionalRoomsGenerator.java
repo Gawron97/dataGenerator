96c38905461156f72aaeea511cff7f090eb31575
MDybek <dybek8780@gmail.com>
2023-11-08 13:46:55 +0100

    added additional rooms description

--- src/main/java/com/example/datagenerator/generator/generators/AdditionalRoomsGenerator.java
+++ src/main/java/com/example/datagenerator/generator/generators/AdditionalRoomsGenerator.java
@@ -14,7 +14,18 @@ public class AdditionalRoomsGenerator extends GeneratorHelper {
 
     private final AdditionalRoomsRepository additionalRoomsRepository;
     private final List<AdditionalRoomType> additionalRoomTypes;
-
+    private final List<String> descriptions = List.of(
+            "Kuchnia wyposażona w sztućce, kubki i talerze oraz czajnik",
+            "Sala kominkowa do spotkań i relaksu",
+            "Sala fitness z nowoczesnym sprzętem do ćwiczeń",
+            "Sala konferencyjna z miejscami siedzącymi dla 50 osób",
+            "Sala gier z bilardem, ping-pongiem i grami planszowymi",
+            "Pracownia komputerowa z dostępem do internetu",
+            "Biblioteka z bogatym zbiorem książek i czasopism",
+            "Pralnia samoobsługowa z pralkami i suszarkami",
+            "Pomieszczenie do przechowywania rowerów",
+            "Kawiarnia z kawą i przekąskami dla mieszkańców"
+    );
     public AdditionalRoomsGenerator(Faker faker, UserRepository userRepository, AdditionalRoomsRepository additionalRoomsRepository, AdditionalRoomTypeRepository additionalRoomTypeRepository) {
         super(faker, userRepository);
         this.additionalRoomsRepository = additionalRoomsRepository;
@@ -30,7 +41,7 @@ public class AdditionalRoomsGenerator extends GeneratorHelper {
         for (int i = 0; i < numberOfAdditionalRooms; i++) {
             AdditionalRooms additionalRooms = AdditionalRooms.builder()
                     .roomNumber(faker.number().numberBetween(1, 110))
-                    .description(null)
+                    .description(descriptions.get(faker.number().numberBetween(0, descriptions.size())))
                     .additionalRoomType(getRandomAdditionalRoomType())
                     .build();
 
