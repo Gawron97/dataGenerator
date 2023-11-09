8d158db04cda5d2001b4ceec2ac79325324e146d
MDybek <dybek8780@gmail.com>
2023-11-09 15:26:00 +0100

    added descriptions to statute and dormitory

--- src/main/java/com/example/datagenerator/generator/generators/DormitoryGenerator.java
+++ src/main/java/com/example/datagenerator/generator/generators/DormitoryGenerator.java
@@ -17,6 +17,18 @@ public class DormitoryGenerator extends GeneratorHelper {
     private final List<String> dormitories =
             List.of("T2", "T3", "T4", "T6", "T15", "T16", "T17", "T18", "T19");
 
+    private final List<String> descriptions = List.of(
+            "Ten akademik to nowoczesny budynek z pełnym wyposażeniem kuchni i łazienek.",
+            "Ten akademik oferuje pokoje jednoosobowe i dwuosobowe z dostępem do internetu.",
+            "Ten akademik znajduje się blisko kampusu i ma wspaniały widok na miasto.",
+            "Ten akademik to doskonałe miejsce do nauki i spotkań towarzyskich.",
+            "Ten akademik oferuje duże sale klubowe i siłownię dla studentów.",
+            "Ten akademik jest przyjazny dla zwierząt i posiada teren rekreacyjny.",
+            "Ten akademik to idealne miejsce dla miłośników sportu z boiskiem i salą fitness.",
+            "Ten akademik zapewnia pełne wyżywienie i ma własną bibliotekę.",
+            "Ten akademik jest dogodnie położony w centrum miasta z dostępem do komunikacji publicznej."
+    );
+
     public DormitoryGenerator(Faker faker, UserRepository userRepository, DormitoryRepository dormitoryRepository,
                               ManagerRepository managerRepository, StatuteRepository statuteRepository) {
         super(faker, userRepository);
@@ -33,7 +45,7 @@ public class DormitoryGenerator extends GeneratorHelper {
                     .name(dormitories.get(i))
                     .city(faker.address().city())
                     .street(faker.address().streetName()).numberOfStreet(faker.address().streetAddressNumber())
-                    .description(null)
+                    .description(descriptions.get(i))
                     .contactInfo(super.getPhoneNumber()).manager(super.getRandomManager(this.managerRepository))
                     .statute(super.getRandomStatute(this.statuteRepository)).build();
 
--- src/main/java/com/example/datagenerator/generator/generators/StatuteGenerator.java
+++ src/main/java/com/example/datagenerator/generator/generators/StatuteGenerator.java
@@ -14,6 +14,12 @@ public class StatuteGenerator extends GeneratorHelper {
     private final StatuteRepository statuteRepository;
 
     private static final List<String> contents = List.of("Ustawa_1", "Ustawa_2", "Ustawa_3", "Ustawa_4");
+    private final List<String> rules = List.of(
+            "Zakaz palenia w pomieszczeniach akademika i na terenie kampusu.",
+            "Obowiązkowa rejestracja gości w recepcji akademika oraz ograniczenie czasu ich pobytu.",
+            "Zakaz hałasu po godzinie 22:00 w celu zapewnienia spokoju innym mieszkańcom.",
+            "Zabrania się trzymania zwierząt domowych w pokojach, chyba że jest to wyraźnie zezwolone."
+    );
 
     public StatuteGenerator(Faker faker, UserRepository userRepository, StatuteRepository statuteRepository) {
         super(faker, userRepository);
@@ -26,7 +32,10 @@ public class StatuteGenerator extends GeneratorHelper {
 
     private Statute createStatute(String content){
 
-        return Statute.builder().content(content).additional_info(null).build();
+        return Statute.builder()
+                .content(content)
+                .additional_info(rules.get(faker.number().numberBetween(0,rules.size())))
+                .build();
 
     }
 
