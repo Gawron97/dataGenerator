ce3f2223b03fb4b7a49067c3189903710d925be5
puzmarta <marta00puz@gmail.com>
2024-01-18 15:53:51 +0100

    Deleted user

--- src/main/java/com/example/datagenerator/mongoDB/repository/UserMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.datagenerator.mongoDB.repository;
-
-import com.example.datagenerator.mongoDB.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserMongoRepository extends MongoRepository<User, String> {
-}
